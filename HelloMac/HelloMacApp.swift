f400e1001c91256288a8cd13db624bfcc0882df8
Daniel Cairoli <daniel.cairoli@icloud.com>
2025-11-21 13:24:34 +0100

    Initial Commit

---
+++ HelloMac/HelloMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMacApp.swift
+//  HelloMac
+//
+//  Created by Daniel on 21.11.25.
+//
+
+import SwiftUI
+
+@main
+struct HelloMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
